bf9f50abc68a579222d37933ef30860906ae7ba8
Mustafizur <sayhemrahman@gmail.com>
2021-12-25 12:43:47 +0600

    added 5 and not by 7

--- MD_MUSTAFIZUR_RAHMAN/g.cpp
+++ MD_MUSTAFIZUR_RAHMAN/g.cpp
@@ -2,3 +2,26 @@
 
 
 //divisible by 5 but not by 7
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int number;
+
+  cin>>number;
+
+  if(number % 5==0 && number %7 !=0)
+  {
+    cout<<"yes";
+  }
+  else
+  {
+    cout<<"No";
+   }
+
+    
+   
+    return 0;
+}
