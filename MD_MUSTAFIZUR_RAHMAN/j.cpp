7f41e82c198dba7c15903489845fef663562a5a2
Mustafizur <sayhemrahman@gmail.com>
2021-12-25 13:43:01 +0600

    added calculator with switch case operator given

--- MD_MUSTAFIZUR_RAHMAN/j.cpp
+++ MD_MUSTAFIZUR_RAHMAN/j.cpp
@@ -1,3 +1,48 @@
 // MD MUSTAFIZUR RAHMAN
 
 // calculator of basic sum, sub, mul, div with operator
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+   char operator;
+
+   double number1, number2;
+   cin>>operator;
+   cin>number1;
+   cin>>number2;
+
+   switch (operator)
+   { 
+       case '+':
+       cout<<number1+number2;
+       break;
+
+       case '-':
+       cout<<number1-number2;
+       break;
+
+       case '*':
+       cout<<number1*number2;
+       break;
+
+       case '/':
+       cout<<number1/number2;
+       break;
+
+       default:
+       cout<<"operator incorrect";
+
+
+
+
+
+
+   }
+
+
+   return 0;
+
+}
