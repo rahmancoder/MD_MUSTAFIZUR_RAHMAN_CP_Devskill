5afe7d072b1609c9dc0a5d2142007ea03bf2f7c7
Mustafizur <sayhemrahman@gmail.com>
2021-12-25 12:29:48 +0600

    added second answer

--- MD_MUSTAFIZUR_RAHMAN/b.cpp
+++ MD_MUSTAFIZUR_RAHMAN/b.cpp
@@ -1,3 +1,16 @@
 // MD MUSTAFIZUR RAHMAN
 
 //square of a number
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int number=7, square;
+    
+    // cin>>number;
+    square=number*number;
+    cout<<square;
+    return 0;
+}
