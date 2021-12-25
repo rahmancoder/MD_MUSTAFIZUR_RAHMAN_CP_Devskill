2d3f43c8529fc8d7afb649c9f01547730d2ae096
Mustafizur <sayhemrahman@gmail.com>
2021-12-25 12:35:28 +0600

    added farhenhit to celcius

---
+++ MD_MUSTAFIZUR_RAHMAN/c.cpp
@@ -0,0 +1,20 @@
+// MD MUSTAFIZUR RAHMAN
+
+// Ferhenhit to celcius
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    float celcius,farhenhit;
+    cin>>farhenhit;
+   
+    cout<<"farhenhit is:"<<farhenhit;
+
+    celcius=(farhenhit-32)* 5/9;
+
+    cout<<"celcius is:"<<celcius;
+    
+   
+    return 0;
+}
