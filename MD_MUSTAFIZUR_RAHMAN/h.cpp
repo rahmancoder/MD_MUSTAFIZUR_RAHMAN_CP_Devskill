e601ea452e92edf2ae9e4c58dfe845215742fd28
Mustafizur <sayhemrahman@gmail.com>
2021-12-25 13:13:10 +0600

    grade calculation with if else

--- MD_MUSTAFIZUR_RAHMAN/h.cpp
+++ MD_MUSTAFIZUR_RAHMAN/h.cpp
@@ -1,3 +1,52 @@
 // MD MUSTAFIZUR RAHMAN
 
 // Grade calculation or MArks calculations
+#include <iostream>
+using namespace std;
+
+int  main() 
+{
+ 
+ int marks;
+ cin>>marks;
+
+ if(marks<0 || marks > 100)
+ {
+     cout<<"invalid marks";
+ }
+ else if(marks >= 0 && marks <= 34)
+ {
+     cout<<"F";
+ }
+  else if(marks >= 35 && marks <= 39)
+ {
+     cout<<"D";
+ }
+  else if(marks >= 40 && marks <= 49)
+ {
+     cout<<"C";
+ }
+ else if(marks >= 50 && marks <= 59)
+ {
+     cout<<"B-";
+ }
+  else if(marks >= 60 && marks <= 59)
+ {
+     cout<<"B+";
+ }
+  else if(marks >= 70 && marks <= 79)
+ {
+     cout<<"A-";
+ }
+  else if(marks >= 80 && marks <= 89)
+ {
+     cout<<"A";
+ }
+else
+cout<<"A+";
+
+
+
+
+       return 0;
+}
