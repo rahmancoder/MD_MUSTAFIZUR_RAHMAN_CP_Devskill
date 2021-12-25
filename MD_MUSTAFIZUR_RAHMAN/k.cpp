772cf3db6f69aa5f141690af9dde2284b050e42e
Mustafizur <sayhemrahman@gmail.com>
2021-12-25 15:47:45 +0600

    added common bits among two binary numbers using right shift

--- MD_MUSTAFIZUR_RAHMAN/k.cpp
+++ MD_MUSTAFIZUR_RAHMAN/k.cpp
@@ -2,3 +2,28 @@
 
 
 // find the set bit of two numbers, how many set bit matched
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+   
+
+   int number1, number2;
+   int count=0;
+   cin>>number1;
+   cin>>number2;
+
+
+   for(int i=0; i<32;i++){
+       if(((number1 >> i)& 1) ==(number2 >> i) & 1)
+       {
+           count++;
+       }
+   }
+   cout<<"common bits:"<<count;
+
+
+   return 0;
+}
