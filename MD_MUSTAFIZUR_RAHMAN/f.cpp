ef3892ed1a2d50eafe057157ec269d100e1acdf6
Mustafizur <sayhemrahman@gmail.com>
2021-12-25 13:01:54 +0600

    converted hour 24 format to 12 format

--- MD_MUSTAFIZUR_RAHMAN/f.cpp
+++ MD_MUSTAFIZUR_RAHMAN/f.cpp
@@ -1,3 +1,50 @@
 // MD MUSTAFIZUR RAHMAN
 
 // 24 hour to 12 hour format
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  string str ="17:35";
+
+  // "17:35"
+  //  01234 =index value, length=5
+
+  int  hour1=(int)str[0]- '0';
+  int  hour2=(int)str[1] - '0';
+
+  int result=hour1 * 10 +h2;
+
+  string Meridien;
+
+  if(result<<12){
+      Meridien="AM";
+  }
+  else{
+      Meridien="PM";
+  }
+
+  result= result % 12;
+
+  if(result==0)
+  {
+      cout<<"12";
+
+      for(int i=2; i<5; ++i){ cout<<str[i];}
+      // printing second portion minute as it is
+  }
+
+  else{
+      cout<< result;
+      for(int i=2; i< 5; ++i){
+          cout<<str[i];
+      }
+  }
+  cout<<Meridien<<"\n";
+
+    
+   
+    return 0;
+}
