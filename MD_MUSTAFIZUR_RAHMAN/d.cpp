c82cec63c78aa039aafb4b655c49abf3d729b28a
Mustafizur <sayhemrahman@gmail.com>
2021-12-25 12:41:05 +0600

    added fourth answer divisible by 5 and 7 or not

--- MD_MUSTAFIZUR_RAHMAN/d.cpp
+++ MD_MUSTAFIZUR_RAHMAN/d.cpp
@@ -1,3 +1,25 @@
 // MD MUSTAFIZUR RAHMAN
 
 // divisible by both 5 and 7
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int number;
+
+  cin>>number;
+
+  if(number % 5==0 && number %7==0)
+  {
+    cout<<"yes";
+  }
+  else
+  {
+    cout<<"No";
+   }
+
+    
+   
+    return 0;
+}
